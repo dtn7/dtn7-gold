217283419cff303164cf8b9f4bf1e71406233fff
Alvar Penning <post@0x21.biz>
2018-12-15 15:21:54 +0100

    CBOR en/decoding for PrimaryBlock

--- bpa/primary_block.go
+++ bpa/primary_block.go
@@ -2,7 +2,10 @@ package bpa
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
+
+	"github.com/ugorji/go/codec"
 )
 
 const DTNVersion uint = 7
@@ -54,6 +57,99 @@ func (pb PrimaryBlock) HasFragmentation() bool {
 // this case the CRC field of this struct should become relevant.
 func (pb PrimaryBlock) HasCRC() bool {
 	return pb.CRCType != CRCNo
+}
+
+func (pb PrimaryBlock) CodecEncodeSelf(enc *codec.Encoder) {
+	var blockArr = []interface{}{
+		pb.Version,
+		pb.BundleControlFlags,
+		pb.CRCType,
+		pb.Destination,
+		pb.SourceNode,
+		pb.ReportTo,
+		pb.CreationTimestamp,
+		pb.Lifetime}
+
+	if pb.HasFragmentation() {
+		blockArr = append(blockArr, pb.FragmentOffset, pb.TotalDataLength)
+	}
+
+	if pb.HasCRC() {
+		blockArr = append(blockArr, pb.CRC)
+	}
+
+	enc.MustEncode(blockArr)
+}
+
+// decodeEndpoints decodes the three defined EndpointIDs. This method is called
+// from CodecDecodeSelf.
+func (pb *PrimaryBlock) decodeEndpoints(blockArr []interface{}) {
+	endpoints := []struct {
+		pos     int
+		pointer *EndpointID
+	}{
+		{3, &pb.Destination},
+		{4, &pb.SourceNode},
+		{5, &pb.ReportTo},
+	}
+
+	for _, ep := range endpoints {
+		var arr []interface{} = blockArr[ep.pos].([]interface{})
+
+		(*ep.pointer).SchemeName = uint(arr[0].(uint64))
+		(*ep.pointer).SchemeSpecificPort = arr[1]
+
+		// The codec library uses uint64 internally but our `dtn:none` is defined
+		// by a more generic uint. In case of an `dtn:none` endpoint we have to
+		// switch the type.
+		if ty := reflect.TypeOf((*ep.pointer).SchemeSpecificPort); ty.Kind() == reflect.Uint64 {
+			(*ep.pointer).SchemeSpecificPort = uint((*ep.pointer).SchemeSpecificPort.(uint64))
+		}
+	}
+}
+
+// decodeCreationTimestamp decodes the CreationTimestamp. This method is called
+// from CodecDecodeSelf.
+func (pb *PrimaryBlock) decodeCreationTimestamp(blockArr []interface{}) {
+	for i := 0; i <= 1; i++ {
+		pb.CreationTimestamp[i] = uint((blockArr[6].([]interface{}))[i].(uint64))
+	}
+}
+
+func (pb *PrimaryBlock) CodecDecodeSelf(dec *codec.Decoder) {
+	var blockArrPt = new([]interface{})
+	dec.MustDecode(blockArrPt)
+
+	var blockArr = *blockArrPt
+
+	if len(blockArr) < 8 || len(blockArr) > 11 {
+		panic("blockArr has wrong length (< 8 or > 10)")
+	}
+
+	pb.decodeEndpoints(blockArr)
+	pb.decodeCreationTimestamp(blockArr)
+
+	pb.Version = uint(blockArr[0].(uint64))
+	pb.BundleControlFlags = BundleControlFlags(blockArr[1].(uint64))
+	pb.CRCType = CRCType(blockArr[2].(uint64))
+	pb.Lifetime = uint(blockArr[7].(uint64))
+
+	switch len(blockArr) {
+	case 9:
+		// CRC, No Fragmentation
+		pb.CRC = uint(blockArr[8].(uint64))
+
+	case 10:
+		// No CRC, Fragmentation
+		pb.FragmentOffset = uint(blockArr[8].(uint64))
+		pb.TotalDataLength = uint(blockArr[9].(uint64))
+
+	case 11:
+		// CRC, Fragmentation
+		pb.FragmentOffset = uint(blockArr[8].(uint64))
+		pb.TotalDataLength = uint(blockArr[9].(uint64))
+		pb.CRC = uint(blockArr[10].(uint64))
+	}
 }
 
 func (pb PrimaryBlock) String() string {
