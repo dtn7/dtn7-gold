473e6c96036c419b6e79e7ff5b1af8da0a17d14f
Alvar Penning <post@0x21.biz>
2018-12-16 16:43:34 +0100

    Extension Blocks

--- bpa/canonical_block.go
+++ bpa/canonical_block.go
@@ -7,6 +7,39 @@ import (
 	"github.com/ugorji/go/codec"
 )
 
+const (
+	// BlockTypePayload is a BlockType for a payload block as defined in 4.2.3.
+	BlockTypePayload uint = 1
+
+	// BlockTypeIntegrity is a BlockType defined in the Bundle Security Protocol
+	// specifiation.
+	BlockTypeIntegrity uint = 2
+
+	// BlockTypeConfidentiality is a BlockType defined in the Bundle Security
+	// Protocol specifiation.
+	BlockTypeConfidentiality uint = 3
+
+	// BlockTypeManifest is a BlockType defined in the Manifest Extension Block
+	// specifiation.
+	BlockTypeManifest uint = 4
+
+	// BlockTypeFlowLabel is a BlockType defined in the Flow Label Extension Block
+	// specification.
+	BlockTypeFlowLabel uint = 6
+
+	// BlockTypePreviousNode is a BlockType for a Previous Node block as defined
+	// in section 4.3.1.
+	BlockTypePreviousNode uint = 7
+
+	// BlockTypeBundleAge is a BlockType for a Bundle Age block as defined in
+	// section 4.3.2.
+	BlockTypeBundleAge uint = 8
+
+	// BlockTypeHopCount is a BlockType for a Hop Count block as defined in
+	// section 4.3.3.
+	BlockTypeHopCount uint = 9
+)
+
 // CanonicalBlock represents the Canonical Bundle Block defined in section 4.2.3
 type CanonicalBlock struct {
 	BlockType         uint
@@ -28,14 +61,6 @@ func NewCanonicalBlock(blockType uint, blockNumber uint,
 		Data:              data,
 		CRC:               0,
 	}
-}
-
-// NewPayloadBlock creates a new payload block based on the CanonicalBlock.
-func NewPayloadBlock(blockControlFlags BlockControlFlags, data interface{}) CanonicalBlock {
-	// Payload block's values are defined in 4.2.3:
-	// - block type: 1
-	// - block number: 0
-	return NewCanonicalBlock(1, 0, blockControlFlags, data)
 }
 
 // HasCRC retruns if the CRCType indicates a CRC present for this block. In
@@ -94,4 +119,45 @@ func (cb CanonicalBlock) String() string {
 	}
 
 	return b.String()
+}
+
+// HopCount represents the tuple of a hop limit and hop count defined in 4.3.3
+// for the Hop Count block.
+type HopCount struct {
+	_struct struct{} `codec:",toarray"`
+
+	Limit uint
+	Count uint
+}
+
+func (hc HopCount) String() string {
+	return fmt.Sprintf("(%d, %d)", hc.Limit, hc.Count)
+}
+
+// NewPayloadBlock creates a new payload block.
+func NewPayloadBlock(blockControlFlags BlockControlFlags, data []byte) CanonicalBlock {
+	// A payload block's block number is always 0 (4.2.3)
+	return NewCanonicalBlock(BlockTypePayload, 0, blockControlFlags, data)
+}
+
+// NewPreviousNodeBlock creates a new Previous Node block.
+func NewPreviousNodeBlock(blockNumber uint, blockControlFlags BlockControlFlags,
+	prevNodeId EndpointID) CanonicalBlock {
+	return NewCanonicalBlock(
+		BlockTypePreviousNode, blockNumber, blockControlFlags, prevNodeId)
+}
+
+// NewBundleAgeBlock creates a new Bundle Age block to hold the bundle's lifetime
+// in microseconds.
+func NewBundleAgeBlock(blockNumber uint, blockControlFlags BlockControlFlags,
+	time uint) CanonicalBlock {
+	return NewCanonicalBlock(
+		BlockTypeBundleAge, blockNumber, blockControlFlags, time)
+}
+
+// NewHopCountBlock creates a new Hop Count block.
+func NewHopCountBlock(blockNumber uint, blockControlFlags BlockControlFlags,
+	hopCount HopCount) CanonicalBlock {
+	return NewCanonicalBlock(
+		BlockTypeHopCount, blockNumber, blockControlFlags, hopCount)
 }
