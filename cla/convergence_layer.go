a20a5ea783c9427533a18687acd45b8c8f483e96
Alvar Penning <post@0x21.biz>
2019-01-08 14:20:38 +0100

    Initial interface for convergence layer adapters

---
+++ cla/convergence_layer.go
@@ -0,0 +1,22 @@
+package cla
+
+import "github.com/geistesk/dtn7/bundle"
+
+// ConvergenceLayer is an interface for convergence layer adapters (CLA). Each
+// CLA should work in an own thread/Goroutine, which is started with the
+// Construct and terminated with the Destruct method. New bundles can be
+// transmitted to a known bundle node, identified by its endpoint ID, by calling
+// the SendBundle method.
+// TODO: reception, threading, ...
+type ConvergenceLayer interface {
+	// TODO: connection to BPA with an observer pattern or the like
+
+	// Construct setups this convergence layer adapter's Goroutine.
+	Construct()
+
+	// Destruct terminates this convergence layer adapter's Goroutine.
+	Destruct()
+
+	// SendBundle transmits the given bundle to a node.
+	SendBundle(recipient bundle.EndpointID, bndl bundle.Bundle)
+}
