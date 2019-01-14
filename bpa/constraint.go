0bb526ab853eba6e1f5580e7ac38f61dd6ffb1f3
Alvar Penning <post@0x21.biz>
2019-01-14 17:08:21 +0100

    retention constraints and a bundle wrapper

---
+++ bpa/constraint.go
@@ -0,0 +1,55 @@
+package bpa
+
+import "github.com/geistesk/dtn7/bundle"
+
+// Constraint is a retention constraint as defined in the subsections of the
+// fifth chapter of draft-ietf-dtn-bpbis-12.
+type Constraint int
+
+const (
+	// DispatchPending is assigned to a bundle if its dispatching is pending.
+	DispatchPending Constraint = iota
+
+	// ForwardPending is assigned to a bundle, if its forwarding is pending.
+	ForwardPending Constraint = iota
+
+	// ReassemblyPending is assigned to a fragmented bundle, if its reassembly is
+	// pending.
+	ReassemblyPending Constraint = iota
+)
+
+// BundlePack is a tuple of a bundle and a set of constraints used in the
+// process of delivering this bundle.
+type BundlePack struct {
+	Bundle      bundle.Bundle
+	Constraints map[Constraint]bool
+}
+
+// NewBundlePack returns a BundlePack for the given bundle.
+func NewBundlePack(b bundle.Bundle) BundlePack {
+	return BundlePack{
+		Bundle:      b,
+		Constraints: make(map[Constraint]bool),
+	}
+}
+
+// HasConstraint returns true if the given constraint contains.
+func (bp BundlePack) HasConstraint(c Constraint) bool {
+	_, ok := bp.Constraints[c]
+	return ok
+}
+
+// HasConstraints returns true if any constraint exists.
+func (bp BundlePack) HasConstraints() bool {
+	return len(bp.Constraints) != 0
+}
+
+// AddConstraint adds the given constraint.
+func (bp *BundlePack) AddConstraint(c Constraint) {
+	bp.Constraints[c] = true
+}
+
+// RemoveConstraint removes the given constraint.
+func (bp *BundlePack) RemoveConstraint(c Constraint) {
+	delete(bp.Constraints, c)
+}
---
+++ bpa/constraint_test.go
@@ -0,0 +1,62 @@
+package bpa
+
+import (
+	"testing"
+
+	"github.com/geistesk/dtn7/bundle"
+)
+
+func TestBundlePackConstraints(t *testing.T) {
+	var bndl, err = bundle.NewBundle(
+		bundle.NewPrimaryBlock(
+			bundle.MustNotFragmented,
+			bundle.MustNewEndpointID("dtn", "dest"),
+			bundle.MustNewEndpointID("dtn", "src"),
+			bundle.NewCreationTimestamp(bundle.DtnTimeEpoch, 0), 24*60*60),
+		[]bundle.CanonicalBlock{
+			bundle.NewBundleAgeBlock(1, bundle.DeleteBundle, 0),
+			bundle.NewPayloadBlock(0, []byte("hello world!")),
+		})
+	if err != nil {
+		t.Errorf("Bundle creation failed: %v", err)
+	}
+
+	var bp = NewBundlePack(bndl)
+
+	if len(bp.Constraints) != 0 {
+		t.Errorf("Inital constraints map is not empty")
+	}
+	if bp.HasConstraints() {
+		t.Errorf("Inital bundle pack has constraints")
+	}
+	if bp.HasConstraint(DispatchPending) {
+		t.Errorf("Inital bundle pack has specific constraint")
+	}
+
+	bp.AddConstraint(DispatchPending)
+
+	if len(bp.Constraints) != 1 {
+		t.Errorf("Bundle pack has wrong length")
+	}
+	if !bp.HasConstraints() {
+		t.Errorf("Bundle pack has no constraints after adding one")
+	}
+	if !bp.HasConstraint(DispatchPending) {
+		t.Errorf("Bundle pack does not have set constraint")
+	}
+	if bp.HasConstraint(ForwardPending) {
+		t.Errorf("Bundle pack has unknown constraint")
+	}
+
+	bp.RemoveConstraint(ForwardPending)
+
+	if len(bp.Constraints) != 1 {
+		t.Errorf("Bundle pack's length after removing unknown constraint")
+	}
+
+	bp.RemoveConstraint(DispatchPending)
+
+	if bp.HasConstraints() {
+		t.Errorf("Bundle pack has constraints after deletion")
+	}
+}
