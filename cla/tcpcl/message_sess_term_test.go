ace7aa39150a4b7cbc18083bff0b42149f2e01d5
Alvar Penning <post@0x21.biz>
2019-09-05 17:08:31 +0200

    TCPCL: Session Termination Message

---
+++ cla/tcpcl/message_sess_term_test.go
@@ -0,0 +1,46 @@
+package tcpcl
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestSessionTerminationMessage(t *testing.T) {
+	t1data := []byte{0x05, 0x00, 0x00}
+	t1message := NewSessionTerminationMessage(0, TerminationUnknown)
+
+	t2data := []byte{0x05, 0x01, 0x01}
+	t2message := NewSessionTerminationMessage(TerminationReply, TerminationIdleTimeout)
+
+	t3data := []byte{0xFF, 0x00, 0x00}
+	t3message := SessionTerminationMessage{}
+
+	tests := []struct {
+		valid bool
+		data  []byte
+		stm   SessionTerminationMessage
+	}{
+		{true, t1data, t1message},
+		{true, t2data, t2message},
+		{false, t3data, t3message},
+	}
+
+	for _, test := range tests {
+		var stm SessionTerminationMessage
+
+		if err := stm.UnmarshalBinary(test.data); (err == nil) != test.valid {
+			t.Fatalf("Error state was not expected; valid := %t, got := %v", test.valid, err)
+		} else if !test.valid {
+			continue
+		} else if !reflect.DeepEqual(test.stm, stm) {
+			t.Fatalf("SessionTerminationMessage does not match, expected %v and got %v", test.stm, stm)
+		}
+
+		if data, err := test.stm.MarshalBinary(); err != nil {
+			t.Fatal(err)
+		} else if !bytes.Equal(data, test.data) {
+			t.Fatalf("Data does not match, expected %x and got %x", test.data, data)
+		}
+	}
+}
