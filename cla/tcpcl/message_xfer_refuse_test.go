c1ef8cb4b30805ca804b18dd09dc50e2f6efa49e
Alvar Penning <post@0x21.biz>
2019-09-05 16:37:02 +0200

    TCPCL: Transfer Refusal Message

---
+++ cla/tcpcl/message_xfer_refuse_test.go
@@ -0,0 +1,56 @@
+package tcpcl
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestTransferRefusalMessage(t *testing.T) {
+	t1data := []byte{
+		// Message Header:
+		0x03,
+		// Reason Code:
+		0x00,
+		// Transfer ID:
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+	}
+	t1message := NewTransferRefusalMessage(RefusalUnknown, 1)
+
+	t2data := []byte{
+		// Message Header:
+		0x05,
+		// Reason Code:
+		0x00,
+		// Transfer ID:
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+	t2message := TransferRefusalMessage{}
+
+	tests := []struct {
+		valid bool
+		data  []byte
+		trm   TransferRefusalMessage
+	}{
+		{true, t1data, t1message},
+		{false, t2data, t2message},
+	}
+
+	for _, test := range tests {
+		var trm TransferRefusalMessage
+
+		if err := trm.UnmarshalBinary(test.data); (err == nil) != test.valid {
+			t.Fatalf("Error state was not expected; valid := %t, got := %v", test.valid, err)
+		} else if !test.valid {
+			continue
+		} else if !reflect.DeepEqual(test.trm, trm) {
+			t.Fatalf("TransferRefusalMessage does not match, expected %v and got %v", test.trm, trm)
+		}
+
+		if data, err := test.trm.MarshalBinary(); err != nil {
+			t.Fatal(err)
+		} else if !bytes.Equal(data, test.data) {
+			t.Fatalf("Data does not match, expected %x and got %x", test.data, data)
+		}
+	}
+}
