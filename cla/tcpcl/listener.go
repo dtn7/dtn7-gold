187a4a25e12ecbb74552ed4fead0732707cc3f63
Alvar Penning <post@0x21.biz>
2019-10-07 16:06:38 +0200

    TCPCL: Server to Listener, bound to cla.Manager

---
+++ cla/tcpcl/listener.go
@@ -0,0 +1,77 @@
+package tcpcl
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/dtn7/dtn7-go/bundle"
+	"github.com/dtn7/dtn7-go/cla"
+)
+
+type TCPCLListener struct {
+	listenAddress string
+	endpointID    bundle.EndpointID
+	manager       *cla.Manager
+
+	stopSyn chan struct{}
+	stopAck chan struct{}
+}
+
+func NewTCPCLListener(listenAddress string, endpointID bundle.EndpointID, manager *cla.Manager) *TCPCLListener {
+	return &TCPCLListener{
+		listenAddress: listenAddress,
+		endpointID:    endpointID,
+		manager:       manager,
+
+		stopSyn: make(chan struct{}),
+		stopAck: make(chan struct{}),
+	}
+}
+
+func (listener *TCPCLListener) Start() error {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", listener.listenAddress)
+	if err != nil {
+		return err
+	}
+
+	ln, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		return err
+	}
+
+	go func(ln *net.TCPListener) {
+		for {
+			select {
+			case <-listener.stopSyn:
+				ln.Close()
+				close(listener.stopAck)
+
+				return
+
+			default:
+				if err := ln.SetDeadline(time.Now().Add(50 * time.Millisecond)); err != nil {
+					log.WithError(err).WithField("cla", listener).Warn(
+						"TCPCLListener failed to set deadline on TCP socket")
+
+					listener.Close()
+				} else if conn, err := ln.Accept(); err == nil {
+					listener.manager.Register(NewTCPCLClient(conn, listener.endpointID))
+				}
+			}
+		}
+	}(ln)
+
+	return nil
+}
+
+func (listener *TCPCLListener) Close() {
+	close(listener.stopSyn)
+	<-listener.stopAck
+}
+
+func (listener TCPCLListener) String() string {
+	return fmt.Sprintf("tcpcl://%s", listener.listenAddress)
+}
