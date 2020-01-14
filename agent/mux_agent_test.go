37dc60f681a3e97d9c7bbef4e6888e812acbb88f
Alvar Penning <post@0x21.biz>
2020-01-14 16:40:25 +0100

    MuxAgent for multiplexing ApplicationAgents

---
+++ agent/mux_agent_test.go
@@ -0,0 +1,91 @@
+package agent
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/dtn7/dtn7-go/bundle"
+)
+
+func TestMuxAgent(t *testing.T) {
+	b1, err := bundle.Builder().
+		Source("dtn://src/").
+		Destination("dtn://agent/mock-1/").
+		CreationTimestampEpoch().
+		Lifetime("24h").
+		BundleAgeBlock(0).
+		HopCountBlock(64).
+		PayloadBlock([]byte("hello world")).
+		Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mux := NewMuxAgent()
+
+	mock1 := newMockAgent([]bundle.EndpointID{bundle.MustNewEndpointID("dtn://agent/mock-1/")})
+	mock2 := newMockAgent([]bundle.EndpointID{bundle.MustNewEndpointID("dtn://agent/mock-2/")})
+
+	mux.Register(mock1)
+	mux.Register(mock2)
+
+	mux.MessageReceiver() <- BundleMessage{b1}
+	time.Sleep(500 * time.Millisecond)
+
+	for i, mock := range []*mockAgent{mock1, mock2} {
+		if msgs := mock.inbox(); len(msgs) != 1 {
+			t.Fatalf("mock agent%d did not receied onemessages; msgs := %v", i, msgs)
+		} else if !reflect.DeepEqual(msgs[0].(BundleMessage).Bundle, b1) {
+			t.Fatalf("message is not b1; %v %v", msgs[0], b1)
+		}
+	}
+
+	mock1.MessageSender() <- ShutdownMessage{}
+	time.Sleep(500 * time.Millisecond)
+
+	select {
+	case msg := <-mux.MessageSender():
+		t.Fatalf("Mux forwarded shutdown message %v", msg)
+
+	case <-time.After(250 * time.Millisecond):
+		break
+	}
+
+	mux.MessageReceiver() <- BundleMessage{b1}
+	time.Sleep(500 * time.Millisecond)
+
+	if msgs := mock1.inbox(); len(msgs) != 0 {
+		t.Fatalf("mock agent1 received messages %v", msgs)
+	}
+
+	if msgs := mock2.inbox(); len(msgs) != 1 {
+		t.Fatalf("mock agent2 did not receied onemessages; msgs := %v", msgs)
+	} else if !reflect.DeepEqual(msgs[0].(BundleMessage).Bundle, b1) {
+		t.Fatalf("message is not b1; %v %v", msgs[0], b1)
+	}
+
+	mock2.send(BundleMessage{b1})
+	time.Sleep(500 * time.Millisecond)
+
+	select {
+	case msg := <-mux.MessageSender():
+		if msg, ok := msg.(BundleMessage); !ok {
+			t.Fatal("Message is no bundle message")
+		} else if !reflect.DeepEqual(msg.Bundle, b1) {
+			t.Fatalf("Expected %v, got %v", b1, msg.Bundle)
+		}
+
+	case <-time.After(250 * time.Millisecond):
+		t.Fatal("Mux did not received message")
+	}
+
+	mux.MessageReceiver() <- ShutdownMessage{}
+	time.Sleep(500 * time.Millisecond)
+
+	if msgs := mock2.inbox(); len(msgs) != 1 {
+		t.Fatalf("mock agent did not received one message; msgs := %v", msgs)
+	} else if !reflect.DeepEqual(msgs[0], ShutdownMessage{}) {
+		t.Fatalf("expected %v, got %v", ShutdownMessage{}, msgs[0])
+	}
+}
