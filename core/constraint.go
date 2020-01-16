512a4c1bdde60418fb316a7ceb008b7c76e169a1
Alvar Penning <post@0x21.biz>
2020-01-16 19:23:43 +0100

    Bump dtn-bpbis to version 19

--- core/constraint.go
+++ core/constraint.go
@@ -1,7 +1,7 @@
 package core
 
 // Constraint is a retention constraint as defined in the subsections of the
-// fifth chapter of draft-ietf-dtn-bpbis-18.
+// fifth chapter of draft-ietf-dtn-bpbis-19.
 type Constraint int
 
 const (
