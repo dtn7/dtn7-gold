667e35fed8ed283abcac5e0548921db2c5f2a45c
Alvar Penning <post@0x21.biz>
2020-08-03 23:40:18 +0200

    Contrib script for REUSE compliance

---
+++ contrib/reuse/reuse-headers.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import subprocess
+
+from datetime import datetime
+from typing import List, Tuple
+
+
+# Rename authors, the following happened due to using GitHub's Web UI.
+AUTHOR_REWRITE = {"Alvar": "Alvar Penning"}
+
+# Exclude unsupported files; there is currently a PR on the way.
+FILE_IGNORE = ["go.mod", "go.sum"]
+
+
+# Ownership is a triple of (Filename, Author, Years)
+Ownership = Tuple[str, str, str]
+
+
+def git(cmds: List[str]) -> str:
+    "Execute a git subcommand, returning stdout as a string."
+
+    proc = subprocess.run(["git"] + cmds,
+            capture_output=True, text=True, check=True)
+    return proc.stdout
+
+
+def file_authors(f: str) -> List[Ownership]:
+    "List all ownerships of the requested file."
+
+    owners_raw = git(["log", "--pretty=format:%an%x00%at", f]).split("\n")
+    owners_tpl = [raw.split(chr(0)) for raw in owners_raw]
+    owners_tpl.reverse()
+
+    owners_year = [(tpl[0], datetime.fromtimestamp(int(tpl[1])).strftime("%Y"))
+            for tpl in owners_tpl]
+
+    owner_grp = dict()
+    for author, year in set(owners_year):
+        if not author in owner_grp:
+            owner_grp[author] = [year]
+        else:
+            owner_grp[author].append(year)
+            owner_grp[author].sort()
+
+    owners = [(f, AUTHOR_REWRITE.get(author, author), ", ".join(years))
+            for (author, years) in owner_grp.items()]
+    sorted(owners, key=lambda owner: owner[1], reverse=True)
+
+    return owners
+
+
+def reuse_addheader(f: str, author: str, years: str, license: str):
+    "Execute `reuse addheader`."
+
+    subprocess.run(["reuse", "addheader",
+        f"--copyright={author}", f"--year={years}", f"--license={license}", f],
+        check=True)
+
+
+if __name__ == "__main__":
+    files = filter(lambda f: f not in FILE_IGNORE, git(["ls-files"]).split())
+
+    owners = []
+    for f in files:
+        owners = owners + file_authors(f)
+
+    for f, author, years in owners:
+        reuse_addheader(f, author, years, "GPL-3.0-or-later")
