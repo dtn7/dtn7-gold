0aeb03c9e06652b555a0b0f98e531989a72f2a5e
Alvar Penning <post@0x21.biz>
2020-08-10 16:35:29 +0200

    Socket Convergence Layer Protocol (SoCLP): Message
    Started work on a layer agnostic Convergence Layer for sockets.
    
    This commit implements SoCLP's Messages and their tests.

---
+++ cla/soclp/message_status.go
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: 2020 Alvar Penning
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package soclp
+
+import (
+	"io"
+
+	"github.com/dtn7/cboring"
+)
+
+const (
+	// StatusShutdown represents a Shutdown Status Message.
+	StatusShutdown uint64 = 0
+
+	// StatusHeartbeat represents a Heartbeat Status Message.
+	StatusHeartbeat uint64 = 1
+)
+
+// StatusMessage informs the peer about some status, e.g., a heartbeat or a scheduled shutdown.
+type StatusMessage struct {
+	StatusCode uint64
+}
+
+// NewShutdownStatusMessage creates a Shutdown Status Message.
+func NewShutdownStatusMessage() *StatusMessage {
+	return &StatusMessage{StatusShutdown}
+}
+
+// NewHeartbeatStatusMessage creates a new Heartbeat Status Message.
+func NewHeartbeatStatusMessage() *StatusMessage {
+	return &StatusMessage{StatusHeartbeat}
+}
+
+// Type code of a StatusMessage is always 1.
+func (sm *StatusMessage) Type() uint64 {
+	return MsgStatus
+}
+
+// IsShutdown checks if this StatusMessage represents a Shutdown Status Message.
+func (sm *StatusMessage) IsShutdown() bool {
+	return sm.StatusCode == StatusShutdown
+}
+
+// IsHeartbeat checks if this StatusMessage represents a Heartbeat Status Message.
+func (sm *StatusMessage) IsHeartbeat() bool {
+	return sm.StatusCode == StatusHeartbeat
+}
+
+func (sm *StatusMessage) String() string {
+	return "StatusMessage"
+}
+
+// MarshalCbor serializes the status code.
+func (sm *StatusMessage) MarshalCbor(w io.Writer) error {
+	return cboring.WriteUInt(sm.StatusCode, w)
+}
+
+// UnmarshalCbor a CBOR-represented status code back to an IdentityMessage.
+func (sm *StatusMessage) UnmarshalCbor(r io.Reader) (err error) {
+	sm.StatusCode, err = cboring.ReadUInt(r)
+	return err
+}
