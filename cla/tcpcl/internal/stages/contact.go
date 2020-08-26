5b7569ec682946f22c35888121df08b8879e719f
Alvar Penning <post@0x21.biz>
2020-08-26 15:17:53 +0200

    TCPCL: Contact Stage for Contact Header exchange

---
+++ cla/tcpcl/internal/stages/contact.go
@@ -0,0 +1,90 @@
+// SPDX-FileCopyrightText: 2020 Alvar Penning
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package stages
+
+import (
+	"fmt"
+
+	"github.com/dtn7/dtn7-go/cla/tcpcl/internal/msgs"
+)
+
+// ContactStage modules the initial ContactHeader exchange.
+type ContactStage struct {
+	state *State
+
+	closeChan chan struct{}
+	finChan   chan struct{}
+}
+
+// Start this Stage based on the previous Stage's State.
+func (cs *ContactStage) Start(state *State) {
+	cs.state = state
+
+	cs.closeChan = make(chan struct{})
+	cs.finChan = make(chan struct{})
+
+	go cs.handle()
+}
+
+func (cs *ContactStage) handle() {
+	if cs.state.Configuration.ActivePeer {
+		cs.handleActive()
+	} else {
+		cs.handlePassive()
+	}
+
+	close(cs.finChan)
+}
+
+func (cs *ContactStage) receiveMsgOrClose() (ch *msgs.ContactHeader, err error) {
+	select {
+	case <-cs.closeChan:
+		err = StageClose
+		return
+
+	case msg := <-cs.state.MsgIn:
+		var ok bool
+		if ch, ok = msg.(*msgs.ContactHeader); !ok {
+			err = fmt.Errorf("received message has invalid type %T", msg)
+			ch = nil
+		}
+
+		return
+	}
+}
+
+func (cs *ContactStage) handleActive() {
+	ch := msgs.NewContactHeader(cs.state.Configuration.ContactFlags)
+	cs.state.MsgOut <- &ch
+
+	if ch, err := cs.receiveMsgOrClose(); err != nil {
+		cs.state.StageError = err
+	} else {
+		cs.state.ContactFlags = ch.Flags
+	}
+}
+
+func (cs *ContactStage) handlePassive() {
+	if ch, err := cs.receiveMsgOrClose(); err != nil {
+		cs.state.StageError = err
+		return
+	} else {
+		cs.state.ContactFlags = ch.Flags
+	}
+
+	ch := msgs.NewContactHeader(cs.state.Configuration.ContactFlags)
+	cs.state.MsgOut <- &ch
+}
+
+// Close this Stage down.
+func (cs *ContactStage) Close() error {
+	close(cs.closeChan)
+	return nil
+}
+
+// Finished closes this channel to indicate this Stage has finished. Afterwards the State should be inspected.
+func (cs *ContactStage) Finished() chan struct{} {
+	return cs.finChan
+}
---
+++ cla/tcpcl/internal/stages/contact_test.go
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: 2020 Alvar Penning
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package stages
+
+import (
+	"testing"
+	"time"
+
+	"github.com/dtn7/dtn7-go/cla/tcpcl/internal/msgs"
+)
+
+func TestContactStage(t *testing.T) {
+	msgIn := make(chan msgs.Message)
+	msgOut := make(chan msgs.Message)
+
+	activeContact := &ContactStage{}
+	activeState := &State{
+		Configuration: Configuration{
+			ActivePeer:   true,
+			ContactFlags: msgs.ContactCanTls,
+		},
+		MsgIn:  msgIn,
+		MsgOut: msgOut,
+	}
+
+	passiveContact := &ContactStage{}
+	passiveState := &State{
+		Configuration: Configuration{
+			ActivePeer:   false,
+			ContactFlags: 0,
+		},
+		MsgIn:  msgOut,
+		MsgOut: msgIn,
+	}
+
+	activeContact.Start(activeState)
+	passiveContact.Start(passiveState)
+
+	finChan := make(chan struct{})
+	go func() { finChan <- <-activeContact.Finished() }()
+	go func() { finChan <- <-passiveContact.Finished() }()
+
+	for fins := 0; fins < 2; {
+		select {
+		case <-finChan:
+			fins += 1
+		case <-time.After(250 * time.Millisecond):
+			t.Fatal("timeout")
+		}
+	}
+
+	if err := activeState.StageError; err != nil {
+		t.Fatal(err)
+	}
+	if err := passiveState.StageError; err != nil {
+		t.Fatal(err)
+	}
+
+	if cf := activeState.ContactFlags; cf != 0 {
+		t.Fatalf("active state's contact flags are %v", cf)
+	}
+	if cf := passiveState.ContactFlags; cf != msgs.ContactCanTls {
+		t.Fatalf("passive state's contact flags are %v", cf)
+	}
+}
